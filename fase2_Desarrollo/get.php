4d2399260910face57429ceb9331c27aaeed2ed4
UO270656 <uo270656@uniovi.es>
2021-06-18 19:46:13 +0200

    cambiandon cosas de la web

---
+++ fase2_Desarrollo/get.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+  <!-- Datos que describen el documento -->
+  <meta charset="UTF-8" />
+  <title>Booksew - Inicio</title>
+  <meta name="Booksew" content="Inicio de la página web Booksew" />
+  <link rel="stylesheet" type="text/css" href="base.css" />
+  <base href="https://uo270656.github.io/SEW_EXTRAORDINARIA/fase2_Desarrollo/get.php" />
+  <!-- añadir el repositorio.io -->
+</head>
+
+<body>
+  <!-- Datos con el contenidos que aparece en el navegador -->
+  <header>
+    <h1 class="principal">
+      <img src="img/logo.png" id="logo" alt="Logo de la página web" />
+    </h1>
+    <nav class="menu">
+      <ul>
+        <li>
+          <a id="inicio" href="inicio.html">Inicio</a>
+        </li>
+        <li>
+          <a id="about" href="about.html">Sobre el autor</a>
+        </li>
+        <li>
+          <a id="faq" href="faq.html">Preguntas frecuentes</a>
+        </li>
+        <li>
+          <a id="xml" href="xml.html">Ver libros xml</a>
+        </li>
+        <li>
+          <a id="add" href="add.php">Agregar datos</a>
+        </li>
+        <li>
+          <a id="get" href="get.php">Ver colección</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <section>
+    <h2>Ver colección</h2>
+    <div></div>
+  </section>
+  <footer>
+    <p id="credits">Copyright @2021 UO270656</p>
+  </footer>
+</body>
+
+</html>
